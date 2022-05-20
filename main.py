e40b66a273e1647cdf6a07df6792a3b110bfb51c
dylansnyk <dylan.havelock@snyk.io>
2022-05-20 14:22:33 -0400

    add verify signature

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import re
 from flask import Flask, jsonify, request
+from util import verify_signature
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/healthcheck", methods=['GET'])
 def hello_world():
-    return jsonify({"hello": "world"})
+    return jsonify({"status": "up"})
 
 @app.route("/event", methods=['POST'])
 def consume_event():
+
+    verify = verify_signature(request)
+
+    print(f'verify: {verify}')
+
     print(request.get_json())
     return jsonify({}), 200
---
+++ util.py
@@ -0,0 +1,19 @@
+import hmac
+import hashlib
+import os
+
+secret = os.environ.get("WEBHOOK_SECRET")
+
+def verify_signature(request):
+
+    byte_key = secret.encode()
+    message = str(request.get_json()).encode()
+    
+    digest = hmac.new( byte_key, message, hashlib.sha256 ).hexdigest()
+
+    signature = f'sha256={digest}'
+
+    print(signature)
+    print(request.headers['x-hub-signature'])
+
+    return signature == request.headers['x-hub-signature']
